c62d334efba89de01785557700b87b82b6a4f56b
Sky <livegogo.16334@gmail.com>
2022-02-06 15:01:48 +0100

    :memo: Update language file and added 4 default items

--- src/main/java/info/itsthesky/itemcreator/core/CreatorCommand.java
+++ src/main/java/info/itsthesky/itemcreator/core/CreatorCommand.java
@@ -1,6 +1,5 @@
 package info.itsthesky.itemcreator.core;
 
-import com.cryptomorin.xseries.XMaterial;
 import info.itsthesky.itemcreator.ItemCreator;
 import info.itsthesky.itemcreator.core.gui.ItemListGUI;
 import info.itsthesky.itemcreator.utils.Utils;
@@ -10,6 +9,7 @@ import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.command.TabCompleter;
 import org.bukkit.enchantments.Enchantment;
+import org.bukkit.entity.EntityType;
 import org.bukkit.entity.Player;
 import org.bukkit.potion.PotionEffectType;
 import org.jetbrains.annotations.NotNull;
@@ -19,7 +19,6 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Locale;
-import java.util.stream.Collectors;
 
 public class CreatorCommand implements CommandExecutor, TabCompleter {
 
@@ -59,6 +58,15 @@ public class CreatorCommand implements CommandExecutor, TabCompleter {
 				new ItemListGUI(ItemCreator.getInstance().getApi().loadAllItems()).open((Player) sender);
 				return true;
 			}
+			case "generate_default" -> {
+				sender.sendMessage(Utils.colored("&6Generating ..."));
+				ItemCreator.getInstance().saveResource("items/cactus_helmet.yml", true);
+				ItemCreator.getInstance().saveResource("items/copper_ingot.yml", true);
+				ItemCreator.getInstance().saveResource("items/killer.yml", true);
+				ItemCreator.getInstance().saveResource("items/golem_spawner.yml", true);
+				sender.sendMessage(Utils.colored("&aSuccess! Item generated in &2plugins/ItemCreator/items/&a !"));
+				return true;
+			}
 			case "list" -> {
 				if (args.length == 1) {
 					sender.sendMessage(Utils.colored("&cEnumeration needed: &6enchantments&c, &6potion_effects"));
@@ -81,6 +89,14 @@ public class CreatorCommand implements CommandExecutor, TabCompleter {
 									+ " &7(ID: "+enchantment.getId()+")"));
 						return true;
 					}
+					case "entities" -> {
+						sender.sendMessage(Utils.colored("&6List of Entities &e("+ EntityType.values().length +")&6:"));
+						for (EntityType enchantment : EntityType.values())
+							sender.sendMessage(Utils.colored("  &6→ &e" +
+									WordUtils.capitalize(enchantment.getName().replace('_', ' ').toLowerCase(Locale.ENGLISH))
+									+ " &7(ID: "+enchantment.name()+")"));
+						return true;
+					}
 				}
 			}
 		}
@@ -96,7 +112,8 @@ public class CreatorCommand implements CommandExecutor, TabCompleter {
 				Utils.colored("&6/ic help &7- &eShow this help page"),
 				Utils.colored("&6/ic get <item id> &7- &eGet the specified custom item"),
 				Utils.colored("&6/ic menu &7- &eOpen the main ItemCreator menu"),
-				Utils.colored("&6/ic list (enchantments|potion_effects) &7- &eList possible values of enumeration"),
+				Utils.colored("&6/ic list (enchantments|potion_effects|entities) &7- &eList possible values of enumeration"),
+				Utils.colored("&6/ic generate_default &7- &eGenerate the default items ItemCreator provide to see everything it can do."),
 				Utils.colored("&1")
 		);
 	}
@@ -105,7 +122,7 @@ public class CreatorCommand implements CommandExecutor, TabCompleter {
 	public @Nullable List<String> onTabComplete(@NotNull CommandSender sender, @NotNull Command command, @NotNull String alias, @NotNull String[] args) {
 		final List<String> completions = new ArrayList<>();
 		if (args.length == 1)
-			completions.addAll(Arrays.asList("help", "get", "menu", "list"));
+			completions.addAll(Arrays.asList("help", "get", "menu", "list", "generate_default"));
 		else {
 			if (args.length == 2) {
 				switch (args[0]) {
@@ -119,7 +136,8 @@ public class CreatorCommand implements CommandExecutor, TabCompleter {
 								.toList());
 						break;
 					case "list":
-						completions.addAll(Arrays.asList("enchantments", "potion_effects"));
+						completions.addAll(Arrays.asList("enchantments", "potion_effects", "entities"));
+						break;
 				}
 			}
 		}
